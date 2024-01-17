d4e56559aeb7c5adace85fadb89f9a2607d55fec
DavidMichaelInnocent <ldsholder3@gmail.com>
2024-01-17 20:23:20 +0100

    new

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -27,7 +27,7 @@ document.querySelector("img").setAttribute("src",picture);
 
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-let favoriteFoods = ["Rice", "Chicken, Yam, Shawama, Eba, Bread, Beans, plantain"];
+let favoriteFoods = ["Rice", "Chicken", "Yam", "Shawama", "Eba", "Bread", "Beans", "plantain"];
 
 // Step 2: place the values of the favorite foods variable into the HTML file
 document.querySelector("#food").textContent = favoriteFoods;
