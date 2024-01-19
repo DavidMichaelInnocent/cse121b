381b653cc3bde8de5f8a2660675e99d0e88eeabd
DavidMichaelInnocent <ldsholder3@gmail.com>
2024-01-19 23:26:55 +0100

    new

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,21 +3,26 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-const fullname = "David Michael Innocent";
+let fullname = "David Michael Innocent";
 
 /* Step 3 - Element Variables */
 
 document.querySelector("#name").textContent = fullname;
 
-const currentyear = "2024";
+let currentyear = "2024";
 
 document.querySelector("#year").textContent = currentyear;
 
-const picture = "images/profile.png";
+let profilepicture = "images/profile.png";
 
 /* Step 4 - Adding Content */
 
-document.querySelector("img").setAttribute("src",picture);
+const nameElement = document.querySelector
+
+document.querySelector("img").setAttribute("src",profilepicture);
+
+
+
 
 
 
@@ -25,31 +30,32 @@ document.querySelector("img").setAttribute("src",picture);
 
 /* Step 5 - Array */
 
+// Declare an array variable to hold your favorite foods
+let favoriteFoods = ["pizza", "ice cream", "salad", "yam", "chicken", "rice", "dodo", "garri"];
 
-// Step 1: declare and instantiate an array variable to hold your favorite foods
-let favoriteFoods = ["Rice", "Chicken", "Yam", "Shawama", "Eba", "Bread", "Beans", "plantain"];
+// Modify the HTML element with the id of food to display your favorite foods array
+let foodElement = document.getElementById("food"); // This is the variable you set in Step 3
+foodElement.innerHTML = favoriteFoods; // This will show the array as a comma-separated string
 
-// Step 2: place the values of the favorite foods variable into the HTML file
-document.querySelector("#food").textContent = favoriteFoods;
+// Declare and instantiate a variable to hold another single favorite food item
+let newFood = ["burger", "eba",];
 
-// Step 3: declare and instantiate a variable to hold another favorite food
-let anotherFavoriteFood = "Cook Beans";
+// Add the value stored in this new variable to your favorite food array
+favoriteFoods.push(newFood); // This will add the new food to the end of the array
 
-// Step 4: add the variable holding another favorite food to the favorite food array
-favoriteFoods.push(anotherFavoriteFood);
+// Append the new array values onto the displayed content of the HTML element with the id of food using a += operator and a <br> (line break character) to provide a line break as shown in Figure 2
+foodElement.innerHTML += "<br>" + favoriteFoods; // This will show the updated array on a new line
 
-// Step 5: repeat Step 2
-document.querySelector("#food").textContent = favoriteFoods;
 
-// Step 6: remove the first element in the favorite foods array
-favoriteFoods.shift();
+// Remove the first element in the favorite food array
+favoriteFoods.shift(); // This will remove the first element ("pizza") from the array
 
-// Step 7: repeat Step 2
-document.querySelector("#food").textContent = favoriteFoods;
+// Again, append the array output showing the modified array, using a line break as shown in step 5.5
+foodElement.innerHTML += "<br>" + favoriteFoods; // This will show the modified array on another new line
 
-// Step 8: remove the last element in the favorite foods array
-favoriteFoods.pop();
+// Remove the last element in the favorite food array
+favoriteFoods.pop(); // This will remove the last element ("burger") from the array
 
-// Step 7: repeat Step 2.
-document.querySelector("#food").textContent = favoriteFoods;
+// Append the array output with this final modified favorite foods array
+foodElement.innerHTML += "<br>" + favoriteFoods; // This will show the final array on the last line
 
