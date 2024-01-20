518e0d9d13df3bcbb0b66449ce78bbfc487f6778
DavidMichaelInnocent <ldsholder3@gmail.com>
2024-01-20 21:53:59 +0100

    today

---
+++ scripts/w03-task.js
@@ -0,0 +1,38 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (Number1, Number2) {}
+return Number1, Number2;
+function addnumbers() {
+    let addend1=parseInt(document.querySelector('#addend1').value); 
+    let addend2=parseInt(document.querySelector('#addend2').value);
+    let result =(addend1, addend2);
+
+}
+let addNumber1 = Number(document.querySelector('#add1').value);
+
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
