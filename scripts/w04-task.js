6c490210f1e774a456f606e4a104e811a0f5d209
DavidMichaelInnocent <ldsholder3@gmail.com>
2024-01-31 23:29:26 +0100

    today

---
+++ scripts/w04-task.js
@@ -0,0 +1,56 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {};
+myProfile.name = "David Michael Innocent";
+
+
+/* Populate Profile Object with placesLive objects */
+
+myProfile.photo = "images/profile.png";
+
+myProfile.favoriteFoods = ["Bread", "Yam", "Milk", "Plantain", "Beans","Water Mello"];
+
+myProfile.hobbies = ["Chatting", "Fathball", "Playing Game"]
+
+myProfile.placelived = [];
+myProfile.placelived.push[{place: "Nigerian, Ibadan", length: "5years"}];
+myProfile.placelived.push[{place: "Ghana, Accra", length: "2years"}];
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.getElementById("name").textContent = myProfile.name;
+
+/* Photo with attributes */
+document.getElementById("photo").src = myProfile.photo;
+document.getElementById("photo").alt = myProfile.name;
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(function(food){
+    let  li = document.createElement("li")
+    li.textContent = food;
+    document.getElementById("favorite-foods").appendChild(li);
+});
+
+/* Hobbies List */
+
+myProfile.hobbies.forEach(function(hobby){
+    let li =  document.createElement("li");
+    li.textContent = hobby;
+    document.getElementById("hobbies").appendChild(li);
+
+});
+
+/* Places Lived DataList */
+myProfile.placelived.forEach(function(place)
+{
+    let dt = document.createElement("dt");
+    dt.textContent = place.place;
+    let dd = document.createElement("dd");
+    dd.textContent =place.length;
+    document.getElementById("places-lived").appendChild(dt);
+    document.getElementById("places-lived").appendChild(dd);
+});
+
