b4d63aba3b7ad586ab060a04f0706e8e1efb9538
DavidMichaelInnocent <ldsholder3@gmail.com>
2024-02-08 19:31:12 +0100

    toady

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,80 +1,98 @@
-/* W05: Programming Tasks */
-
-/* Declare and initialize global variables */
+// Declare a const variable named templesElement that references the HTML div element that eventually will be populated with temple data.
 const templesElement = document.getElementById("temples");
 
+// Declare a global empty array variable to store a list of temples named templeList.
 let templeList = [];
 
+// Declare a function expression using const named displayTemples that uses an arrow function to accept a list of temples as an array argument.
+const displayTemples = (temples) => {
+  // Process each temple in the temple array using a forEach method and do the following for each temple item:
+  temples.forEach((temple) => {
+    // Create an HTML <article> element (createElement).
+    let article = document.createElement("article");
 
-/* async displayTemples Function */
-const displayTemples =(temples) => {
-    temples.forEach((temple) => {
-        let article = document.createElement("article");
+    // Create an HTML <h3> element and add the temple's templeName property to this new element.
+    let h3 = document.createElement("h3");
+    h3.textContent = temple.templeName;
 
-        let h3 = document.createElement("h3");
-        h3.textContent = temple.templeName;
+    // Create an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's location property to the alt attribute.
+    let img = document.createElement("img");
+    img.src = temple.imageUrl;
+    img.alt = temple.location;
 
-        let img = document.createElement("img");
-        img.src = temple.imageUrl; 
-        img.alt = temple.location;
+    // Append the <h3> element and the <img> element to the <article> element as children. (appendChild)
+    article.appendChild(h3);
+    article.appendChild(img);
 
-        article.appendChild(h3);
-        article.appendChild(img);
-        templesElement.appendChild(article);
-    });
+    // Append the <article> element to the global templesElement variable declared in Step 2.
+    templesElement.appendChild(article);
+  });
 };
 
-
-/* async getTemples Function using fetch()*/
+// Create another function expression called getTemples. Make it an async anonymous, arrow function.
 const getTemples = async () => {
-    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+  // In the function, declare a const variable named response that awaits the built-in fetch method calling the temple data, absolute URL given in Step 2 above.
   
-    templeList = await response.json();
+const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
 
-    displayTemples(templeList);
+  // Convert your fetch response into a JavaScript object (.json) and assign the result to the templeList global array variable you declared in Step 3 above. Make sure the the execution of the code waits here as well until it finishes.
+  templeList = await response.json();
+
+  // The last statement in the getTemples function calls the displayTemples function and passes it the list of temples (templeList).
+  displayTemples(templeList);
 };
 
-/* reset Function */
+// Declare a function expression named reset that clears all of the <article> elements from the templesElement.
 const reset = () => {
-    while (templesElement.firstChild){
-        templesElement.removeChild(templesElement.firstChild);
-    }
+  // Use a while loop to remove the first child of the templesElement until it has no more children.
+  while (templesElement.firstChild) {
+    templesElement.removeChild(templesElement.firstChild);
+  }
 };
 
+// Declare a function expression named filterTemples.
+const filterTemples = (temples) => {
+  // In this function, first call the reset function to clear the output.
+  reset();
 
-/* filterTemples Function */
-let sortBy = function (temples)  {
+  // Define a variable named filter that obtains the value of the HTML element with the ID of filtered (The pull-down menu).
+  let filter = document.getElementById("filtered").value;
 
-    reset();
-
-    let filter = document.getElementById("filtered").value;
-    switch (filter) {
-        case "utah":
-
-        displayTemples(temples.filter((temple) => temple.location.includes("utah")));
-        break;
-
-        case "notutah":
-            displayTemples(temples.filter((temple) => temple.location.includes("notutah")));
-
-        break;
-
-        case "older":
-        displayTemples(temples.filter(temple => temple.dedicated < new Date(1950, 0, 1)));
-
-        break;
-
-        case "all":
-            displayTemples(temples);
-        break; 
-        default:
-            alert("invalid filter option");
-    }
+  // Use a switch statement that uses the filter value as the selector responding to four (4) cases.
+  switch (filter) {
+    // For each case, call the displayTemples function using an filter statement that filters the temples parameter for the four options provided.
+    case "utah":
+      // "utah": filter for temples where the location contains "Utah" as a string.
+      displayTemples(temples.filter((temple) => temple.location.includes("Utah")));
+      break;
+    case "notutah":
+      // "nonutah": filter for temples where the location does not contain "Utah" as a string.
+      displayTemples(
+        temples.filter((temple) => !temple.location.includes("Utah"))
+      );
+      break;
+    case "older":
+      // "older": filter for temples where the dedicated date is before 1950. (compare versus new Date(1950, 0, 1)).
+      displayTemples(
+        temples.filter(
+          (temple) => new Date(temple.dedicated) < new Date(1950, 0, 1)
+        )
+      );
+      break;
+    case "all":
+      // "all": no filter. Just use temples as the argument.
+      displayTemples(temples);
+      break;
+    default:
+      // If none of the cases match, display an error message.
+      alert("Invalid filter option");
+  }
 };
 
+// Add a change event listener to the HTML element with an ID of filtered that calls the filterTemples function and sends a arrow function result with the templeList as the argument.
+document.getElementById("filtered").addEventListener("change", () => {
+  filterTemples(templeList);
+});
 
-
-/* Event Listener */
-document.getElementById("filtered").addEventListener("change", () => { filterTemples(templeList) });
-
+// Test your code by calling the getTemples function at the end of your JavaScript file.
 getTemples();
